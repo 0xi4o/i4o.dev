591ace2bb76f204c89fd7fc0e0fa308a132197cb
Ilango <rajagopalilango@gmail.com>
2023-02-13 23:53:47 +0530

    📦 NEW: Add tailwind and prettier

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./app/**/*.{js,jsx,ts,tsx,md,mdx}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}

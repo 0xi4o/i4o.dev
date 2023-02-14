d9eb2ca354c4920fba5d4162ae58dfce366b6495
Ilango <rajagopalilango@gmail.com>
2023-02-14 23:40:01 +0530

    👌 IMPROVE: Update home page

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function Card(props: Props) {
 	return (
-		<div className='inline-flex cursor-pointer items-center justify-center rounded-md border border-zinc-100 bg-transparent p-4 transition-all duration-300 hover:bg-zinc-100 dark:border-zinc-700/40 hover:dark:bg-zinc-800'>
+		<div className='flex cursor-pointer items-center justify-center rounded-md border border-zinc-100 bg-transparent p-4 transition-all duration-300 hover:bg-zinc-100 dark:border-zinc-700/40 hover:dark:bg-zinc-800'>
 			{props.children}
 		</div>
 	)

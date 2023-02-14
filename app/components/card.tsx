15c3fa150a64cec2d5adc5a84a495d7d18254b20
Ilango <rajagopalilango@gmail.com>
2023-02-14 14:46:54 +0530

    📦 NEW: Add card component

---
+++ app/components/card.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+interface Props {
+	children: ReactNode
+}
+
+function Card(props: Props) {
+	return (
+		<div className='inline-flex cursor-pointer items-center justify-center rounded-md border border-zinc-100 bg-transparent p-4 transition-all duration-300 hover:bg-zinc-300 dark:border-zinc-700/40 hover:dark:bg-zinc-800'>
+			{props.children}
+		</div>
+	)
+}
+
+export default Card

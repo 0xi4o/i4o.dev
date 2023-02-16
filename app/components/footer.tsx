219e6a851623b54d086591432c8dfa531a33960c
Ilango <rajagopalilango@gmail.com>
2023-02-17 01:15:07 +0530

    📦 NEW: Add navigation to navbar and footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,4 +1,29 @@
+import { Nav } from '@i4o-oss/catalystui'
+
 function Footer() {
+	const navItems = [
+		{
+			id: 'about',
+			label: 'About',
+			href: '/about',
+		},
+		{
+			id: 'uses',
+			label: 'Uses',
+			href: '/uses',
+		},
+		{
+			id: 'twitter',
+			label: 'Twitter',
+			href: 'https://twitter.com/i4o_dev',
+		},
+		{
+			id: 'github',
+			label: 'Github',
+			href: 'https://github.com/i4o-dev',
+		},
+	]
+
 	return (
 		<div className='mt-32'>
 			<div className='flex items-center justify-center'>
@@ -9,7 +34,9 @@ function Footer() {
 								<div className='flex items-center gap-2 text-xs text-gray-900 dark:text-gray-400'>
 									<p>Ilango Rajagopal © 2023</p>
 								</div>
-								<div className='flex items-center justify-end gap-4'></div>
+								<div className='flex items-center justify-end gap-4'>
+									<Nav items={navItems} />
+								</div>
 							</div>
 						</div>
 					</div>
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@remix-run/react'
-import { Switch } from '@i4o-oss/catalystui'
+import { Nav, Switch } from '@i4o-oss/catalystui'
 import { Theme, useTheme } from '~/utils/theme'
 
 const config = {
@@ -17,6 +17,29 @@ function Navbar() {
 		)
 	}
 
+	const navItems = [
+		{
+			id: 'about',
+			label: 'About',
+			href: '/about',
+		},
+		{
+			id: 'projects',
+			label: 'Projects',
+			href: '/projects',
+		},
+		{
+			id: 'blog',
+			label: 'Blog',
+			href: '/blog',
+		},
+		{
+			id: 'now',
+			label: 'Now',
+			href: '/now',
+		},
+	]
+
 	return (
 		<header className='flex h-24 w-screen flex-wrap items-center justify-center px-4 py-4 sm:px-6 lg:px-8'>
 			<div className='flex w-full max-w-5xl items-center justify-between'>
@@ -26,6 +49,7 @@ function Navbar() {
 					</Link>
 				</div>
 				<div className='flex flex-grow items-center justify-end gap-4'>
+					<Nav items={navItems} />
 					{config.navbar?.search && (
 						<input
 							className='h-10 w-80 rounded-md bg-zinc-200 px-4 py-1 text-sm dark:bg-zinc-800 dark:text-gray-300'

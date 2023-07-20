8415898b6c053aba7b866029de5ebf0a2e8673d4
Ilango <rajagopalilango@gmail.com>
2023-07-20 09:43:22 +0530

    👌 IMPROVE: Update remix config to use future flags

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,21 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+	postcss: true,
+	tailwind: true,
 	future: {
-		unstable_postcss: true,
-		unstable_tailwind: true,
+		v2_dev: true,
+		v2_errorBoundary: true,
+		v2_headers: true,
+		// TODO: update to v2_meta
+		// v2_meta: true,
+		v2_normalizeFormMethod: true,
+		v2_routeConvention: true,
 	},
 	ignoredRouteFiles: ['**/.*'],
 	// appDirectory: "app",
 	// assetsBuildDirectory: "public/build",
 	// serverBuildPath: "api/index.js",
 	// publicPath: "/build/",
+	serverModuleFormat: 'cjs',
 	serverDependenciesToBundle: [/.*/],
 }

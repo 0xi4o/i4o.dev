8512ebe806095f2eeb5b2cf52bae9d9f32a5f5d9
Ilango <rajagopalilango@gmail.com>
2023-07-23 11:52:33 +0530

    👌 IMPROVE: Revert deploy target changes

--- remix.config.js
+++ remix.config.js
@@ -1,24 +1,21 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
-	devServerBroadcastDelay: 1000,
-	ignoredRouteFiles: ['**/.*'],
-	server: './server.ts',
-	serverBuildPath: 'functions/[[path]].js',
-	serverConditions: ['workerd', 'worker', 'browser'],
-	serverDependenciesToBundle: 'all',
-	serverMainFields: ['browser', 'module', 'main'],
-	serverMinify: true,
-	serverModuleFormat: 'esm',
-	serverPlatform: 'neutral',
-	postcss: true,
-	tailwind: true,
-	future: {
-		v2_dev: true,
-		v2_errorBoundary: true,
-		v2_headers: true,
-		// TODO: update to v2_meta
-		// v2_meta: true,
-		v2_normalizeFormMethod: true,
-		v2_routeConvention: true,
-	},
-}
+module.exports = {
+    postcss: true,
+    tailwind: true,
+    future: {
+        v2_dev: true,
+        v2_errorBoundary: true,
+        v2_headers: true,
+        // TODO: update to v2_meta
+        // v2_meta: true,
+        v2_normalizeFormMethod: true,
+        v2_routeConvention: true,
+    },
+    ignoredRouteFiles: ["**/.*"],
+    // appDirectory: "app",
+    // assetsBuildDirectory: "public/build",
+    // serverBuildPath: "api/index.js",
+    // publicPath: "/build/",
+    serverModuleFormat: "cjs",
+    serverDependenciesToBundle: [/.*/],
+};

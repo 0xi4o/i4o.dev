a9f95c3c1ba497ea8e16b87d0062a0547e45b17e
Ilango <rajagopalilango@gmail.com>
2023-10-04 18:42:27 +0530

    👌 IMPROVE: Update root

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react'
 import type {
 	LinksFunction,
-	LoaderArgs,
+	LoaderFunctionArgs,
 	MetaFunction,
 	SerializeFrom,
 } from '@remix-run/node'
@@ -14,12 +14,17 @@ import {
 	Scripts,
 	ScrollRestoration,
 	useLoaderData,
+	useLocation,
 } from '@remix-run/react'
 import { ThemeHead, ThemeProvider, useTheme } from '~/utils/theme'
 import { getThemeSession } from '~/utils/theme.server'
-import { Layout } from '~/components'
 import styles from '~/main.css'
 import cuiStyles from '@i4o/catalystui/main.css'
+import Navbar from './components/Navbar'
+import BlurCircle from '~/components/BlurCircle'
+import theme from '~/data/theme'
+import Footer from './components/Footer'
+import { getCurrentTrack } from '~/utils/helpers'
 
 export const links: LinksFunction = () => [
 	{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -51,32 +56,88 @@ export const links: LinksFunction = () => [
 	{ rel: 'mask-icon', href: '/safari-pinned-tab.svg' },
 ]
 
-export const meta: MetaFunction = () => ({
-	charset: 'utf-8',
-	viewport: 'width=device-width,initial-scale=1',
-	'msapplication-TileColor': '#2b5797',
-	'og:site': 'https://i4o.dev',
-	'og:url': 'https://i4o.dev',
-	'og:title': 'Ilango',
-	'og:description': '',
-	'og:image': '',
-	'theme-color': '#ffffff',
-	title: 'Ilango',
-	'twitter:card': 'summary_large_image',
-	'twitter:site': '@i4o_dev',
-	'twitter:url': 'https://i4o.dev/',
-	'twitter:creator': '@i4o_dev',
-	'twitter:title': 'Ilango',
-	'twitter:description': '',
-	'twitter:image': '',
-})
+export const meta: MetaFunction = () => [
+	{
+		charSet: 'utf-8',
+	},
+	{
+		name: 'msapplication-TileColor',
+		content: '#2cb67d',
+	},
+	{
+		property: 'og:site',
+		content: 'https://i4o.dev',
+	},
+	{
+		property: 'og:url',
+		content: 'https://i4o.dev',
+	},
+	{
+		property: 'og:title',
+		content: 'Personal site and blog of Ilango Rajagopal',
+	},
+	{
+		property: 'og:description',
+		content: 'Software Engineer, Maker, Writer',
+	},
+	{
+		property: 'og:image',
+		content: '',
+	},
+	{
+		name: 'theme-color',
+		content: '#090909',
+	},
+	{
+		title: 'Ilango Rajagopal - Software Engineer, Maker, Writer',
+	},
+	{
+		name: 'description',
+		content: 'Software Engineer, Maker, Writer',
+	},
+	{
+		name: 'twitter:card',
+		content: 'summary_large_image',
+	},
+	{
+		name: 'twitter:site',
+		content: '@0xi4o',
+	},
+	{
+		name: 'twitter:url',
+		content: 'https://i4o.dev/',
+	},
+	{
+		name: 'twitter:creator',
+		content: '@0xi4o',
+	},
+	{
+		name: 'twitter:title',
+		content: 'Personal site and blog of Ilango Rajagopal',
+	},
+	{
+		name: 'twitter:description',
+		content: 'Software Engineer, Maker, Writer',
+	},
+	{
+		name: 'twitter:image',
+		content: '',
+	},
+	{
+		name: 'viewport',
+		content: 'width=device-width,initial-scale=1',
+	},
+]
 
 export type LoaderData = SerializeFrom<typeof loader>
 
-export const loader = async ({ request }: LoaderArgs) => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const nowPlayingTrack = await getCurrentTrack()
+	const data = await nowPlayingTrack.json()
 	const themeSession = await getThemeSession(request)
 
 	return json({
+		spotify: data,
 		theme: themeSession.getTheme(),
 	})
 }
@@ -95,14 +156,35 @@ function Document({ children }: { children: ReactNode }) {
 				<Links />
 				<ThemeHead ssrTheme={Boolean(data.theme)} />
 			</head>
-			<body className='h-full w-full bg-zinc-50 dark:bg-[#040303]'>
+			<body className='mx-auto flex min-h-screen max-w-[872px] flex-col gap-9 bg-neutral-950 px-10 py-8 font-sans text-sm text-neutral-400 md:gap-20 md:py-16'>
 				{process.env.NODE_ENV === 'production' ? (
-					<script
-						async
-						src='https://analytics.i4o.dev/script.js'
-						data-website-id='a23700f3-870e-47f9-8912-52e806ef4d43'
-					></script>
+					<>
+						<script
+							async
+							src='https://analytics.i4o.dev/script.js'
+							data-website-id='a23700f3-870e-47f9-8912-52e806ef4d43'
+						></script>
+					</>
 				) : null}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+        	                window.CustomSubstackWidget = {
+    	                        substackUrl: 'ilango.substack.com',
+                                placeholder: 'paul@house-atreides.com',
+                            	buttonText: 'Subscribe',
+   	                    	    theme: 'custom',
+       	            	        colors: {
+           	    	                primary: '#FFFFFF',
+           	    	                input: '#000000',
+           	        	            email: '#FFFFFF',
+      	                	        text: '#000000',
+   	                        	},
+                           }
+                       `,
+					}}
+				/>
+				<script src='https://substackapi.com/widget.js' async />
 				{children}
 				<ScrollRestoration />
 				<Scripts />
@@ -113,11 +195,16 @@ function Document({ children }: { children: ReactNode }) {
 }
 
 function App() {
+	const data = useLoaderData<LoaderData>()
+	const location = useLocation()
+
 	return (
 		<Document>
-			<Layout>
-				<Outlet />
-			</Layout>
+			<Navbar data={data.spotify} pathname={location.pathname} />
+			<BlurCircle position='top' color={theme.colors.blur.top} />
+			<BlurCircle position='bottom' color={theme.colors.blur.bottom} />
+			<Outlet />
+			<Footer />
 		</Document>
 	)
 }

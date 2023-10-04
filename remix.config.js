c1df8808ae6e19cd0c460249fdaf8fd85ea75f49
Ilango <rajagopalilango@gmail.com>
2023-10-04 18:34:58 +0530

    👌 IMPROVE: Update packages to latest version

--- remix.config.js
+++ remix.config.js
@@ -1,21 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-    postcss: true,
-    tailwind: true,
-    future: {
-        v2_dev: true,
-        v2_errorBoundary: true,
-        v2_headers: true,
-        // TODO: update to v2_meta
-        // v2_meta: true,
-        v2_normalizeFormMethod: true,
-        v2_routeConvention: true,
-    },
-    ignoredRouteFiles: ["**/.*"],
-    // appDirectory: "app",
-    // assetsBuildDirectory: "public/build",
-    // serverBuildPath: "api/index.js",
-    // publicPath: "/build/",
-    serverModuleFormat: "cjs",
-    serverDependenciesToBundle: [/.*/],
-};
+	postcss: true,
+	tailwind: true,
+	ignoredRouteFiles: ['**/.*'],
+	// appDirectory: "app",
+	// assetsBuildDirectory: "public/build",
+	// serverBuildPath: "api/index.js",
+	// publicPath: "/build/",
+	serverModuleFormat: 'cjs',
+	serverDependenciesToBundle: [/.*/],
+}

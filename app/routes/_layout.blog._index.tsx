e49e3d667c60b32a001a4cb4cb1b5c2875ba3ce2
Ilango <rajagopalilango@gmail.com>
2023-10-20 10:33:17 +0530

    👌 IMPROVE: Update blog page layout and styles

--- app/routes/_layout.blog._index.tsx
+++ app/routes/_layout.blog._index.tsx
@@ -18,46 +18,54 @@ export default function Blog() {
 	)
 
 	return (
-		<>
-			<h1 className='mb-8 font-serif text-3xl'>Blog</h1>
-			{sortedYears.map((year) => (
-				<div
-					className='mb-4 flex flex-col items-start gap-8'
-					key={`year${year}`}
-				>
-					{currentYear !== Number(year) ? (
-						<h2 className='m-0 mt-8 font-serif text-2xl'>{year}</h2>
-					) : null}
-					<div className='flex flex-col items-start gap-4'>
-						{
-							// @ts-ignore
-							postsGroupedByYear[year].map(
-								(post: Post, index: number) => (
-									<div
-										className='flex flex-wrap items-end justify-start gap-2 text-left'
-										key={`post${index}`}
-									>
-										<Link
-											className='no-underline'
-											to={`/blog/${post.slug}`}
-										>
-											<h3 className='m-0 font-sans text-base leading-6 hover:text-neutral-100'>
-												{post.title}
-											</h3>
-										</Link>
-										<span className='text-xs italic'>
-											{format(
-												new Date(post.date),
-												'MMM dd'
-											)}
-										</span>
-									</div>
-								)
-							)
-						}
-					</div>
-				</div>
-			))}
-		</>
+		<main className='flex flex-col gap-20'>
+			<article className='flex flex-col gap-8'>
+				<h1 className='font-serif text-3xl text-neutral-100'>Blog</h1>
+			</article>
+			<article className='flex flex-col gap-4'>
+				<section className='flex flex-col gap-4'>
+					{sortedYears.map((year) => (
+						<div
+							className='mb-4 flex flex-col items-start gap-8'
+							key={`year${year}`}
+						>
+							{currentYear !== Number(year) ? (
+								<h2 className='m-0 mt-8 font-serif text-2xl'>
+									{year}
+								</h2>
+							) : null}
+							<div className='flex flex-col items-start gap-4'>
+								{
+									// @ts-ignore
+									postsGroupedByYear[year].map(
+										(post: Post, index: number) => (
+											<div
+												className='flex flex-wrap items-end justify-start gap-2 text-left'
+												key={`post${index}`}
+											>
+												<Link
+													className='no-underline'
+													to={`/blog/${post.slug}`}
+												>
+													<h3 className='m-0 font-sans text-base leading-6 hover:text-neutral-100'>
+														{post.title}
+													</h3>
+												</Link>
+												<span className='text-xs italic'>
+													{format(
+														new Date(post.date),
+														'MMM dd'
+													)}
+												</span>
+											</div>
+										)
+									)
+								}
+							</div>
+						</div>
+					))}
+				</section>
+			</article>
+		</main>
 	)
 }

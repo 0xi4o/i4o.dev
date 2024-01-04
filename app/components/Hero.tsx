64c4539d52380582b3da838261650701e6a73fd3
Ilango <rajagopalilango@gmail.com>
2024-01-04 09:56:28 +0530

    👌 IMPROVE: Update hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,21 +1,18 @@
-import presentation from '~/data/presentation'
 import SocialLinks from '~/components/SocialLinks'
-import { convertAsteriskToStrongTag } from '~/utils/helpers'
 
 export default function Hero() {
 	return (
 		<article className='flex flex-col gap-8'>
 			<h1 className='font-serif text-3xl text-neutral-100'>
-				{presentation.title}
+				Hi, I’m Ilango 👋
 			</h1>
-			<p
-				className='prose prose-invert max-w-[60ch] leading-6'
-				dangerouslySetInnerHTML={{
-					__html: convertAsteriskToStrongTag(
-						presentation.description
-					),
-				}}
-			/>
+			<p className='prose prose-invert max-w-[60ch] leading-6'>
+				Hello, I'm a <strong>software engineer</strong> from Bangalore,
+				India. I build
+				<strong>open-source</strong> apps and tools with{' '}
+				<strong>Remix, React, TypeScript, Elixir, and Rust</strong>. I
+				write about web development, indiehacking, and personal growth.
+			</p>
 			<SocialLinks />
 		</article>
 	)
--- app/data/presentation.ts
+++ app/data/presentation.ts
@@ -6,17 +6,12 @@ type Social = {
 type Presentation = {
 	logo: string
 	mail: string
-	title: string
-	description: string
 	socials: Social[]
 }
 
 const presentation: Presentation = {
 	logo: 'https://i4o.dev/logo.png',
 	mail: 'hey@i4o.dev',
-	title: 'Hi, I’m Ilango 👋',
-	description:
-		"Hello, I'm a *software engineer* from Bangalore, India. I build *open-source* apps and tools with *Remix, React, TypeScript, and Rust*. I write about web development, indiehacking, and personal growth.",
 	socials: [
 		{
 			label: 'Twitter',

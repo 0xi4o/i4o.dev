16083de9d076a40c224660aed173bde677502da9
Ilango <rajagopalilango@gmail.com>
2024-02-17 13:06:23 +0530

    🐛 FIX:

--- app/utils/mdx.server.tsx
+++ app/utils/mdx.server.tsx
@@ -1,4 +1,4 @@
-import { promises as fs } from 'node:fs'
+import { promises as fs } from 'fs'
 import path from 'node:path'
 import matter from 'gray-matter'
 import remarkFrontmatter from 'remark-frontmatter'

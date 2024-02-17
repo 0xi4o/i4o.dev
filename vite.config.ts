e4f1ffe73d31a45377fe43f85d539ca0d3646ece
Ilango <rajagopalilango@gmail.com>
2024-02-17 12:29:08 +0530

    👌 IMPROVE: Update vite and vite config and add cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
-import { installGlobals } from '@remix-run/node'
 import mdx from '@mdx-js/rollup'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { unstable_vitePlugin as remix } from '@remix-run/dev'
-
-installGlobals()
+import {
+	unstable_vitePlugin as remix,
+	unstable_cloudflarePreset as cloudflare,
+} from '@remix-run/dev'
 
 export default defineConfig({
 	plugins: [
@@ -15,7 +15,13 @@ export default defineConfig({
 		}),
 		remix({
 			ignoredRouteFiles: ['**/.*'],
+			presets: [cloudflare()],
 		}),
 		tsconfigPaths(),
 	],
+	ssr: {
+		resolve: {
+			externalConditions: ['workerd', 'worker'],
+		},
+	},
 })
